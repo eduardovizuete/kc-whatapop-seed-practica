3607bfc6ccc8c74182557fe6d19e92951a24d57b
Vizuete Eduardo <vizuete.eduardo@gmail.com>
2016-12-15 20:51:55 -0500

    Blue Path: Apañando la fecha de publicación

--- app/pipes/publication-date.pipe.ts
+++ app/pipes/publication-date.pipe.ts
@@ -1,3 +1,4 @@
+import { Pipe, PipeTransform } from "@angular/core";
 import * as moment from "moment";
 import "moment/locale/es";
 
@@ -12,3 +13,14 @@ import "moment/locale/es";
 | tienes que usarla donde proceda. Haciendo                        |
 | 'moment(fecha).fromNow()' obtenemos justo lo que necesitamos.    |
 |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+@Pipe({
+    name: "fechaPublicacion"
+})
+export class PublicationDatePipe implements PipeTransform {
+
+    transform(fechaDada: string): string {
+        return moment(fechaDada).fromNow(); 
+    }
+
+}
